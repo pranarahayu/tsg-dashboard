7e34ea5fdd64557e3d8627a64fbd15db1446ba02
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-08-28 10:17:45 +0700

    Update 2_Weekly_Data.py

--- pages/2_Weekly_Data.py
+++ pages/2_Weekly_Data.py
@@ -1,22 +1,21 @@
-import streamlit as st
+import sys
 import pandas as pd
-from datetime import date, timedelta
+import numpy as np
+import streamlit as st
+import io
 
-def run():
-    st.set_page_config(
-        page_title="Home Page",
-    )
+import openpyxl, yattag
+from openpyxl import load_workbook
 
-    st.write("# Selamat datang di Dashboard Internal TSG!")
-
-    st.markdown(
-        """
-        Placeholder.
-    """
-    )
-
-if __name__ == "__main__":
-    run()
+st.set_page_config(page_title='Weekly Data', layout='wide')
+st.markdown('# Weekly Data')
 
 from menu import menu
 menu()
+
+col1, col2 = st.columns(2)
+with col1:
+    gw = st.selectbox('Select GW', [1,2,3,4], key='2')
+    all_gws = st.checkbox('Select All GWs', key='5')
+with col2:
+    mat = st.selectbox('Select Match', ['Match1','Match2'], key='3')
