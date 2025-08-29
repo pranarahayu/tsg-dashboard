079050faf5ac5d2177533f60955e09189af0e07e
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-08-29 10:22:52 +0700

    Update 2_Weekly_Data.py

--- pages/2_Weekly_Data.py
+++ pages/2_Weekly_Data.py
@@ -13,9 +13,17 @@ st.markdown('# Weekly Data')
 from menu import menu
 menu()
 
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    xlsx_url = sheets_url.replace("/edit#gid=", "/export?format=xlsx&gid=")
+    return pd.read_excel(xlsx_url)
+df = load_data(st.secrets["matchdata"])
+
 col1, col2 = st.columns(2)
 with col1:
     gw = st.selectbox('Select GW', [1,2,3,4], key='2')
     all_gws = st.checkbox('Select All GWs', key='5')
 with col2:
     mat = st.selectbox('Select Match', ['Match1','Match2'], key='3')
+
+st.write(df)
