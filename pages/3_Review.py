661bdc07103406bab3e79a3bfa93f4ea97628e32
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-09-09 11:20:39 +0700

    Update 3_Review.py

--- pages/3_Review.py
+++ pages/3_Review.py
@@ -2,21 +2,8 @@ import streamlit as st
 import pandas as pd
 from datetime import date, timedelta
 
-def run():
-    st.set_page_config(
-        page_title="Home Page",
-    )
-
-    st.write("# Selamat datang di Dashboard Internal TSG!")
-
-    st.markdown(
-        """
-        Placeholder.
-    """
-    )
-
-if __name__ == "__main__":
-    run()
+st.set_page_config(page_title='Review', layout='wide')
+st.markdown('# Review')
 
 from menu import menu
 menu()
