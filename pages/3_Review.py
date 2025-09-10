9f5f3179d06f109073acf7372ec20bcacc4a83fa
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2025-09-10 13:39:18 +0700

    Update 3_Review.py

--- pages/3_Review.py
+++ pages/3_Review.py
@@ -5,5 +5,18 @@ from datetime import date, timedelta
 st.set_page_config(page_title='Review', layout='wide')
 st.markdown('# Review')
 
+col1, col2, col3, col4 = st.columns(4)
+with col1:
+  komp = st.selectbox('Select Competition', ['Liga 1', 'Liga 2'], key='0')
+
+with col2:
+  pekan = st.multiselect('Select Gameweek', ['Liga 1', 'Liga 2'], key='1')
+
+with col3:
+  klub = st.selectbox('Select Club', ['Liga 1', 'Liga 2'], key='2')
+
+with col4:
+  ven = st.multiselect('Select Venue', ['Home', 'Away'], key='3')
+
 from menu import menu
 menu()
