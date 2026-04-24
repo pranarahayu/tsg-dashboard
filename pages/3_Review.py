0457cece6b56a2c9e59336041a7c1e20c2ec9291
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2026-04-24 09:39:01 +0700

    Update 3_Review.py

--- pages/3_Review.py
+++ pages/3_Review.py
@@ -24,20 +24,21 @@ df = load_data(st.secrets["matchdata"])
 df2 = load_data(st.secrets["timeline"])
 db = load_data(st.secrets["dbase"])
 
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 with col1:
     komp = st.selectbox('Select Competition', ['Super League', 'Championship'], key='0')
     temp = df[df['Kompetisi']==komp].reset_index(drop=True)
+with col2:
+    jns = st.selectbox('Select Data', ['Match Data', 'Event Data'], key='2')
 with col2:
     pekan = st.multiselect('Select Gameweek', pd.unique(temp['Gameweek']), key='1')
     all_gws = st.checkbox('Select All GWs', key='2')
 if all_gws:
     pekan = df['Gameweek'].unique().tolist()
 
-datas = findata(temp, db, pekan)
 buffer = io.BytesIO()
 with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
     datas.to_excel(writer, sheet_name='Sheet1', index=False)
 download = st.download_button(label="Download data as Excel", data=buffer.getvalue(),
-                              file_name='Match-Data.xlsx', mime='application/vnd.ms-excel')
-st.write(datas)
+                              file_name=jns+'_'+komp+'.xlsx', mime='application/vnd.ms-excel')
+st.write(jns)
