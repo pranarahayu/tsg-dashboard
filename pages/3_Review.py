98da11fb879c785c6d330699988bedb5ba336924
Prana A. Rahayu <109712781+pranarahayu@users.noreply.github.com>
2026-04-24 11:15:33 +0700

    Update 3_Review.py

--- pages/3_Review.py
+++ pages/3_Review.py
@@ -32,9 +32,9 @@ with col2:
     jns = st.selectbox('Select Data', ['Match Data', 'Event Data'], key='3')
 with col3:
     if komp == 'Super League':
-        pekan = st.multiselect('Select Gameweek', [x for x in range(1,35)]), key='1')
+        pekan = st.multiselect('Select Gameweek', [x for x in range(1,35)], key='1')
     else:
-        pekan = st.multiselect('Select Gameweek', [x for x in range(1,28)]), key='4')
+        pekan = st.multiselect('Select Gameweek', [x for x in range(1,28)], key='4')
     all_gws = st.checkbox('Select All GWs', key='2')
 if all_gws:
     pekan = df['Gameweek'].unique().tolist()
